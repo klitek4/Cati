7234cf68a024b3ed2615f120e0270be95e6cf953
Kvcurek <81987976+Kvcurek@users.noreply.github.com>
2023-02-08 23:14:36 +0100

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-const { Discord, Intents, Client } = require('discord.js')
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
-
-const token = "token"
-client.on("ready") => {
-console.log(`Zalogowano na ${client.user.tag}`)}
